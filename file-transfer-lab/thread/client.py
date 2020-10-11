1c232e4840bfeee14751aed1b33834de9dc089a8
galvarez6 <35929189+galvarez6@users.noreply.github.com>
2020-10-11 17:11:30 -0600

    Update client.py

--- file-transfer-lab/thread/client.py
+++ file-transfer-lab/thread/client.py
@@ -1,3 +1,39 @@
 #! /usr/bin/env python3
 
 import socket, sys, re
+#! /usr/bin/env python3
+
+import socket, sys, re
+sys.path.append("../../lib")
+import params
+from os import path
+from os.path import exists
+
+from encapFramedSock import EncapFramedSock
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False),
+    (('-?', '--usage'), "usage", False),
+    )
+
+progname = "testClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Cant parse Server: port from '%s'" % server)
+    sys.exit(1)
+
+addrFamily = socket.AF_INET
+socktype = socket.SOCK_STREAM
+addrPort = (serverHost, serverPort)
+
+sock = socket.socket(addrFamily, socktype)
