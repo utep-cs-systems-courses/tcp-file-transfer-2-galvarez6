2b35b910eda8db8939774420bf7d25b856a8bbba
Gilbert <galvarez6@miners.utep.edu>
2020-10-11 17:27:36 -0600

    working on client

--- file-transfer-lab/thread/client.py
+++ file-transfer-lab/thread/client.py
@@ -1,9 +1,6 @@
 #! /usr/bin/env python3
 
 import socket, sys, re
-#! /usr/bin/env python3
-
-import socket, sys, re
 sys.path.append("../../lib")
 import params
 from os import path
@@ -37,3 +34,28 @@ socktype = socket.SOCK_STREAM
 addrPort = (serverHost, serverPort)
 
 sock = socket.socket(addrFamily, socktype)
+
+if sock is None:
+    print('could not open socket')
+    sys.exit(1)
+
+#connect to server 
+sock.connect(addrPort)
+#let encapFramedSock handle receiving files
+fsock = EncapFramedSock((sock, addrPort))
+
+for i in range(1):
+    #let the user type the file they want to send to the server
+    fileToSend = input("enter file name:")
+
+    #open the file in read binary to send as binary to the server
+    file = open(fileToSend, 'rb')
+    
+    #payload contains the msg from the file to send to the server
+    payload = file.read()
+    
+    #if there is no more data read from the file it has been sent to the server
+    #in my previous i didnt have a payload it was just read in read binary
+
+    #send payload to server
+    fsock.send(payload)
