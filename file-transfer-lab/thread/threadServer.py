a6ff9e1b915ce2acaacfc85d95b713c3d20fdcbe
galvarez6 <galvarez6@miners.utep.edu>
2020-10-18 20:33:24 -0600

    working on locking files from being using at the same time with threads:

--- file-transfer-lab/thread/threadServer.py
+++ file-transfer-lab/thread/threadServer.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append("../../lib")
 import re, socket, params, os
 from os.path import exists
+from threading import Thread, enumerate, Lock
 
 switchesVarDefaults = (
     (('-l', '--listenPort'), 'listenPort', 50001),
@@ -39,6 +40,8 @@ fileLockList = []
 #just in case more threads run at the same time and append to the list
 #however instead of pop() we can use remove(value)
 #it removes the first element of the list with that same value
+global locker
+locker = Lock()
 
 
 class Server(Thread):
@@ -57,26 +60,41 @@ class Server(Thread):
             #if not append it so we know what files are being used and are locked
             if(fileNameFromC not in fileLockList):
                 fileLockList.append(fileNameFromC)
+
+                print("LOCKING FILE: "+fileNameFromC)
+                locker.acquire()
+                #####lock this critical area for threads that dont have a lock so they cannot access this part
+                payload = self.fsock.receive()
+                #client sent the name of the file first so we got that
+                #we can use the name to lock the file descriptor while its being used
+                #we can put the name of the file on a dictionary/list/array object and if other threads
+                #want to write to that same file they check if it is on the stack
+                #if it is they have to wait for the lock
+                #if not the we give the lock to the thread that wants to
+                #write to that file on the server with that name
+                #and when the thread is done remove the file name from the file dictionary/list/array object
+                print("got file: " + fileFromClient.decode())
+                print("contents of file: " + payload.decode())
+                #write to new file on server
+                #check if file exists
+                #this will influence the file name by placing a number next to it
+
+                fileNew = str(numFiles)+fileFromClient.decode()
+                serverFile = open(fileNew, 'wb')
+                serverFile.write(payload)
+                serverFile.close()
+                #done with this file close it and pop it from the list so it can be used again
+                #our own method
+                fileLockList.remove(fileNameFromC)
+                locker.release()
+                print("UNLOCKING FILE: "+fileNameFromC)
+                ######end of lock
             #else if the file is in the list we have to make the thread wanting to use it wait
             #to acquire the lock
 
 
 
-            #####lock this critical area for threads that dont have a lock so they cannot access this part
-            payload = self.fsock.receive()
-            #client sent the name of the file first so we got that
-            #we can use the name to lock the file descriptor while its being used
-            #we can put the name of the file on a dictionary/list/array object and if other threads
-            #want to write to that same file they check if it is on the stack
-            #if it is they have to wait for the lock
-            #if not the we give the lock to the thread that wants to
-            #write to that file on the server with that name
-            #and when the thread is done remove the file name from the file dictionary/list/array object
-            print("got file: " + fileFromClient.decode())
-            print("contents of file: " + payload.decode())
-            #write to new file on server
-            #check if file exists
-            #this will influence the file name by placing a number next to it
+
 
             ######
             #for now this is useless
@@ -84,18 +102,11 @@ class Server(Thread):
                 #numFiles = numFiles + 1
             ####
 
-            fileNew = str(numFiles)+fileFromClient.decode()
-            serverFile = open(fileNew, 'wb')
-            serverFile.write(payload)
-            serverFile.close()
-            #done with this file close it and pop it from the list so it can be used again
-            #our own method
-            fileLockList.remove(fileNameFromC)
-            ######end of lock
 
 
 
-            #thrash but dont wanna get rid of 
+
+            #thrash but dont wanna get rid of
             # #get the name of the file and message from the testClient
             # #the file name will be used to write to a new file of the same
             # #name on the server side
